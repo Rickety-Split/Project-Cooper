38e0b554a8fecab442638ce1cc43502bae138a5a
rickety-split <135549595+Rickety-Split@users.noreply.github.com>
2023-09-20 20:19:24 +0800

    Format server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,57 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-app.listen(8081, ()=> {
-    console.log("Listening.....");
-})
+// Create a MySQL database connection
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'your-password', // Replace with your MySQL password
+  database: 'dbCooper', // Replace with your database name
+});
+
+// Connect to the database
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+  } else {
+    console.log('Connected to MySQL database');
+  }
+});
+
+// Define a route to fetch all users
+app.get('/api/users', (req, res) => {
+  const query = 'SELECT * FROM Users';
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Error fetching users:', err);
+      res.status(500).json({ error: 'Unable to fetch users' });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+// Define a route to fetch a single user by ID
+app.get('/api/users/:id', (req, res) => {
+  const userId = req.params.id;
+  const query = 'SELECT * FROM Users WHERE ID = ?';
+  db.query(query, [userId], (err, results) => {
+    if (err) {
+      console.error('Error fetching user:', err);
+      res.status(500).json({ error: 'Unable to fetch user' });
+    } else {
+      if (results.length === 0) {
+        res.status(404).json({ error: 'User not found' });
+      } else {
+        res.json(results[0]);
+      }
+    }
+  });
+});
+
+// Other CRUD routes for creating, updating, and deleting users can be added here
+
+// Start the server
+app.listen(8081, () => {
+  console.log('Server is listening on port 8081');
+});
