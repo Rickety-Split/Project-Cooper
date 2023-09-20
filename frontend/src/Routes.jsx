9e822191a9087091ad0c84e858d4931d1e8c22e8
rickety-split <135549595+Rickety-Split@users.noreply.github.com>
2023-09-20 20:48:41 +0800

    Fix Routes.jsx, UpdateUserForm.jsx, and UserList.jsx compile errors

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,20 +1,20 @@
 // src/Routes.jsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import UserList from './components/UserList';
-import CreateUserForm from './components/CreateUserForm';
-import UpdateUserForm from './components/UpdateUserForm';
+import { BrowserRouter as Router, Route, Routes as ReactRoutes} from 'react-router-dom';
+import UserList from '../../frontend/src/UserList';
+import CreateUserForm from '../../frontend/src/CreateUserForm';
+import UpdateUserForm from '../../frontend/src/UpdateUserForm';
 
 function Routes() {
     return (
         <Router>
-            <Switch>
+            <ReactRoutes>
                 {/* Define routes for different views */}
                 <Route exact path="/" component={UserList} />
                 <Route path="/create" component={CreateUserForm} />
                 <Route path="/update/:id" component={UpdateUserForm} />
                 {/* Add more routes for other components as needed */}
-            </Switch>
+            </ReactRoutes>
         </Router>
     );
 }
--- frontend/src/UpdateUserForm.jsx
+++ frontend/src/UpdateUserForm.jsx
@@ -84,3 +84,5 @@ function UpdateUserForm() {
         </div>
     );
                 }
+
+export default UpdateUserForm;
